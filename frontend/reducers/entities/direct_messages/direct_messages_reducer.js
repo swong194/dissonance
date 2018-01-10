620822b6191b4ce95c67707ec94d98d05d33d991
Sunny Wong <swong194@gmail.com>
2018-01-09 19:13:13 -0500

    adds direct messages reducer

--- frontend/reducers/entities/direct_messages/direct_messages_reducer.js
+++ frontend/reducers/entities/direct_messages/direct_messages_reducer.js
@@ -1,15 +1,17 @@
+import { RECEIVE_DIRECT_MESSAGES, RECEIVE_DIRECT_MESSAGE } from '../../../actions/direct_message_actions';
 
 const oldState = {};
 
 const DirectMessagesReducer = (state = oldState, action)=>{
 
-  // switch (action.type) {
-  //   case expression:
-  //
-  //     break;
-  //   default:
-  //
-  // }
+  switch (action.type) {
+    case RECEIVE_DIRECT_MESSAGES:
+      return action.messages;
+    case RECEIVE_DIRECT_MESSAGE:
+      return Object.assign({}, state, {[action.message.id]: action.message});
+    default:
+      return state;
+  }
 };
 
 export default DirectMessageReducer;
