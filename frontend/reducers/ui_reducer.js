979e9f80fbbea8c28a3048da5b236c6ab4c91227
Sunny Wong <swong194@gmail.com>
2018-01-11 08:55:26 -0500

    prevent modal closing on receiving messages

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_MODAL, REMOVE_MODAL } from '../actions/ui_actions';
+import { RECEIVE_MESSAGE } from '../actions/message_actions';
 
 const oldState = { serverFormModalOpen: false,
   updateServerModal: false,
@@ -11,6 +12,8 @@ const UiReducer = (state = oldState, action) => {
   switch (action.type) {
     case RECEIVE_MODAL:
       return Object.assign({}, oldState, {[action.modalType]: true});
+    case RECEIVE_MESSAGE:
+      return state;
     default:
       return oldState;
   }
