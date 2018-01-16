53f30c7434eff24f4225b05f9e8094d0a75a0a0d
Sunny Wong <swong194@gmail.com>
2018-01-15 19:11:25 -0500

    edit font family for errors and removed logger and updates webpack config

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 
 const configureStore = (preLoadedState = {}) => {
   return createStore(
     RootReducer,
     preLoadedState,
-    applyMiddleware(thunk, logger));
+    applyMiddleware(thunk));
 };
 
 export default configureStore;
