697419bc55d63c33d5b8b61886752865dba7935e
Sunny Wong <swong194@gmail.com>
2018-01-25 15:14:20 -0500

    removed preset states in ui reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -2,14 +2,6 @@ import { RECEIVE_MODAL, REMOVE_MODAL } from '../actions/ui_actions';
 import { RECEIVE_MESSAGE } from '../actions/message_actions';
 import { RECEIVE_ERRORS, REMOVE_ERRORS } from '../actions/error_actions';
 
-// const oldState = { serverFormModalOpen: false,
-//   updateServerModal: false,
-//   deleteServerModal: false,
-//   openChannelModal: false,
-//   openMessageModal: false,
-//   openTextchannelModal: false
-// };
-
 const oldState = {};
 
 const UiReducer = (state = oldState, action) => {
