e96319bd22ad29def3a6d43bf01302da437c784f
Sunny Wong <swong194@gmail.com>
2018-01-26 16:39:45 -0500

    removed connect from App component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,24 +7,21 @@ import {
   HashRouter,
   withRouter
 } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import SessionFormContainer from './session/session_form_container';
 import GreetingContainer from './greeting/greeting_container';
 import ServerIndexContainer from './server/server_index_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
-class App extends React.Component{
-  render(){
-    return (
-      <Switch>
-        <AuthRoute path='/login' component={SessionFormContainer} />
-        <AuthRoute path='/signup' component={SessionFormContainer} />
-        <ProtectedRoute path='/servers' component={ServerIndexContainer} />
-        <Route exact path='/' component={GreetingContainer}/>
-      </Switch>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Switch>
+      <AuthRoute path='/login' component={SessionFormContainer} />
+      <AuthRoute path='/signup' component={SessionFormContainer} />
+      <ProtectedRoute path='/servers' component={ServerIndexContainer} />
+      <Route exact path='/' component={GreetingContainer}/>
+    </Switch>
+  );
+};
 
-export default withRouter(connect(null, null)(App));
+export default App;
