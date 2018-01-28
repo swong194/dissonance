68c1c0ccb4abfc65fb5c2c6bd63c8504eb873406
Sunny Wong <swong194@gmail.com>
2018-01-28 09:48:02 -0500

    adds tab for server name on hover

--- frontend/components/server/server_index_item.jsx
+++ frontend/components/server/server_index_item.jsx
@@ -24,6 +24,11 @@ class ServerIndexItem extends React.Component{
           to={`/servers/${this.props.server.id}/textChannel/${this.props.server.text_channels[0]}`}>
           <p>{this.props.server.name[0].toUpperCase()}</p>
         </NavLink>
+        <div className='server-item-name'>
+          <div></div>
+          <p>{this.props.server.name.length > 15 ? this.props.server.name.slice(0,13) + '...' : this.props.server.name}</p>
+        </div>
+        <div className='triangle'></div>
       </div>
     );
   }
